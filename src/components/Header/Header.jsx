aef462487cb07da03b2525ec345c52f25d4068f2
Caio Cabral <ocaiocabral@gmail.com>
2023-01-05 14:09:53 -0400

    Ligh and dark mode

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
+import { MoonIcon, SunIcon } from '@heroicons/react/24/solid';
+import { useEffect } from 'react';
+
+
 const Header = () => {
+   const toggleTheme = () => {
+      document.documentElement.classList.toggle("dark");
+   }
+
+   useEffect(() => 
+      {if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+         document.documentElement.classList.add("dark")
+      }
+   }, []);   
+
    return(
       <div className="flex h-20 bg-gray-400 justify-between items-center px-5">
          <span className="text-gray-100">Oi, internauta</span>
          <h1 className="text-gray-100 text-xl">Newsletter</h1>
-         <input className="hidden sm:block" type="checkbox"/>
+         {/* <input className="hidden sm:block" type="checkbox"/> */}
+         <MoonIcon className="h-8 text-gray-100 block cursor-pointer dark:hidden" onClick={toggleTheme}/>
+         <SunIcon className="h-8 text-gray-100 hidden cursor-pointer dark:block" onClick={toggleTheme} />
       </div>
    )
 }
