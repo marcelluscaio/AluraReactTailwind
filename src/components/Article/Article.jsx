8364f8b83d8906ff34bdd04844cbee18bdc51ded
Caio Cabral <ocaiocabral@gmail.com>
2023-01-06 14:50:19 -0400

    Criada secao de artigos

---
+++ src/components/Article/Article.jsx
@@ -0,0 +1,14 @@
+const Article = ({title, text, tags, image, alt}) => {
+   return(
+      <article className="p-5 bg-gray-200 dark:bg-dark-200 rounded-xl shadow-md flex flex-col items-center gap-4">
+         
+         <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold">{title}</h3>
+         <div className="self-end">
+            {tags.map((tag, key) => {return <span key={key} className="bg-alura-100 dark:bg-dark-100 rounded-full text-white px-4 py-2 mr-2 uppercase text-xs">{tag}</span>})}
+         </div>
+         <p className="dark:text-white">{text}</p>
+      </article>
+   )
+}
+
+export default Article
--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -1,9 +1,14 @@
 import data from '../../../articles.json';
+import Article from '../Article/Article'
 
 const ArticlesList = () => {
    return(
-      <div className='mt-5 grid gap-5 m-auto max-w-2xl sm:grid-cols-2 lg:grid-cols-3'>
-         {data.map(article => <p>{article.title}</p>)}
+      <div className='mt-5 grid gap-5 m-auto max-w-[85%] sm:grid-cols-2 lg:grid-cols-3'>
+         {data.map((article, key) => <Article key={key}
+            title = {article.title}
+            text = {article.text}
+            tags = {article.tags}
+         />)}
       </div>
    )
 }
