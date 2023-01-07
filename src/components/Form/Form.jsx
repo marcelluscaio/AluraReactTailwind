2b2630505ba0eceafaeac2b6128022f9d73909c7
Caio Cabral <ocaiocabral@gmail.com>
2023-01-07 11:47:40 -0400

    Form styling when invalid

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,12 +5,14 @@ const Form = ({setUser}) => {
       const password = e.target[1].value;
       setUser({name: name, password: password})
    }
+
+   const styles = "pl-5 py-1 border-2 border-alura-100 dark:border-white dark:bg-dark-100 dark:text-white rounded-full outline-none w-full max-w-2xl invalid:focus:border-red-400"
    
    return(
-      <form onSubmit={safeSubmit}>
-         <input type="text"/>
-         <input type="password"/>
-         <button>Submit</button>
+      <form className="flex flex-col items-center gap-10 mt-40 mx-20 h-full" onSubmit={safeSubmit}>
+         <input className={styles} required type="text"/>
+         <input className={styles} required type="password"/>
+         <button className="bg-alura-100 dark:bg-dark-200 text-white px-6 py-2 w-full rounded-full max-w-lg hover:animate-bounce">Submit</button>
       </form>
    )
 };
