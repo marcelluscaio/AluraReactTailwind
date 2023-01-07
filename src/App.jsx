17f136e5d48837472217aa3aee5ca45c2344c2c8
Caio Cabral <ocaiocabral@gmail.com>
2023-01-07 10:22:56 -0400

    Creation of form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
+
 import Header from './components/Header/Header';
 import ArticlesList from './components/ArticleList/ArticleList';
+import Form from './components/Form/Form'
 
 const App = () => {
+  const [user, setUser] = useState();
+  
   return(
     <>
       <Header/>
-      <ArticlesList/>
+      {user && <ArticlesList/>}
+      {user || <Form />}
     </>
   )
 }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,11 @@
+const Form = () => {
+   return(
+      <form>
+         <input type="text"/>
+         <input type="password"/>
+         <button></button>
+      </form>
+   )
+};
+
+export default Form;
